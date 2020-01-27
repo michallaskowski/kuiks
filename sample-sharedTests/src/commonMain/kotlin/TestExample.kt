93abfb51cfa690ef1fdaecfe883106f95f7a023a
Michal Laskowski <michal@michallaskowski.dev>
2020-01-27 23:03:18 +0100

    wip

--- sample-sharedTests/src/commonMain/kotlin/TestExample.kt
+++ sample-sharedTests/src/commonMain/kotlin/TestExample.kt
@@ -1,12 +1,14 @@
 import com.laskowski.kuiks.ApplicationWrapper
 import com.laskowski.kuiks.Platform
 import com.laskowski.kuiks.platform
+import kotlin.test.Test
 
 class TestExample {
 
     val identifier: String
         get() = if (platform == Platform.iOS) "" else "MainActivity"
 
+    @Test
     fun testOne() {
         val app = ApplicationWrapper(identifier)
         app.launch()
