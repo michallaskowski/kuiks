2d3f843e88f4063dd5ec4ce3d5ee8b8f3ccd5c53
Michal Laskowski <michal@michallaskowski.dev>
2020-02-08 16:37:38 +0100

    Add sample-ios

---
+++ sample-ios/SampleiOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleiOS
+//
+//  Created by mlaskowski on 08/02/2020.
+//  Copyright © 2020 Michal Laskowski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
